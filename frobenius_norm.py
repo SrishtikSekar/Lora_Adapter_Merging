4c89ae13dd8d0684bd6123893f337d7a2006091b
SrishtikSekar <srishtik23bcs220@iiitkottayam.ac.in>
2026-06-17 11:21:09 +0530

    Added frobenius Norm

---
+++ frobenius_norm.py
@@ -0,0 +1,17 @@
+def frobenius_norm(deltas):
+    model_norms={}
+    for layer_name,delta in deltas:
+        model_norms[layer_name]=delta.norm(p="fro").item()
+    import matplotlib.pyplot as plt
+
+    layers=list(model_norms.keys())
+    values=list(model_norms.values())
+
+    plt.figure(figsize=(14,6))
+    plt.plot(range(len(values)),values)
+    plt.xticks(range(len(values)),layers,rotation=90)
+    plt.ylabel("Frobenius Norm")
+    plt.xlabel("Layer")
+    plt.title("Layer wise LoRA Update Strength")
+    plt.tight_layout()
+    plt.show()
